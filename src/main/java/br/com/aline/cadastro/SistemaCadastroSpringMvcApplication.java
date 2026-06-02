01d5f08e2e8af5dd929fabc8aa1b4e1cfa6b8d9f
Aline Figueiredo Souto <alinefigsou@gmail.com>
2026-06-02 20:55:01 -0300

    scaffold

---
+++ src/main/java/br/com/aline/cadastro/SistemaCadastroSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.aline.cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCadastroSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCadastroSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aline/cadastro/SistemaCadastroSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aline.cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCadastroSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
